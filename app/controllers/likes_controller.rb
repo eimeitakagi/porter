7071de1ddf86f87569e468f9f6b9b7475bd9b418
13044032 <13043069@gakushuin.ac.jp>
2015-11-17 22:53:39 +0900

    enable to connect /lanking

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,19 @@
 class LikesController < ApplicationController
 
-  # イイね
-  def like
-    @note = Note.find(params[:note_id])
-    like = current_user.likes.build(note_id: @note.id)
-    like.save
-  end
+# イイね
+def like
+	@note = Note.find(params[:note_id])
+	like = current_user.likes.build(note_id: @note.id)
+	like.save
+end
 
-  # イイね取り消し
-  def unlike
-    @note = Note.find(params[:note_id])
-    like = current_user.likes.find_by(note_id: @note.id)
-    like.destroy
-  end
+	# イイね取り消し
+	def unlike
+		@note = Note.find(params[:note_id])
+		like = current_user.likes.find_by(note_id: @note.id)
+		like.destroy
+	end
+	def index
+		@notes = Note.all
+	end
 end
