cffe5abb315b52c7adce54c9f118b0d20e1b34c1
13044032 <13043069@gakushuin.ac.jp>
2015-12-05 17:20:32 +0900

    検索機能を使いやすくしました

--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 20151102061114) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
-    t.integer  "like_count"
-    t.integer  "access_count"
   end
 
   create_table "users", force: :cascade do |t|
